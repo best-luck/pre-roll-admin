542ff5cf842b86e9984231bad639c1cafff1c232
Ubuntu <user@user-ubuntu>
2024-03-07 12:38:37 -0500

    banner image upload

--- src/lib/functions/server/helper.ts
+++ src/lib/functions/server/helper.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export const saveBufferToFile = (location: string, buffer: Buffer) => {
   return new Promise((resolve: any, reject: any) => {
-    fs.writeFile(path.join(process.cwd(), location), buffer, (error) => {
+    fs.writeFile(location, buffer, (error) => {
       if (error) {
         reject(error);
       } else {
