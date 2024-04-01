dc46fa80a2898058b9197a571997c60d77cf1031
unknown <webmaster0407@gmail.com>
2024-04-01 20:17:34 +0900

    product grid layout completed

--- src/components/shared/pages/products/product/listing.tsx
+++ src/components/shared/pages/products/product/listing.tsx
@@ -46,56 +46,57 @@ export default function ProductListing(props: ProductProps) {
 
   return (
     <div className="product-wrapper">
-      <div className="product border-gray-300">
+      <div className="product border-gray-300 flex flex-col justify-between">
       {
         isFetching ?
           <ProductPulse /> :
           <>
-            <div className="product-image mr-5">
+            <div className="product-image">
               <Link href={`/shop/product/${props.product.slug}`}>
                 <Image
                   src={props.product.image}
                   alt=""
                   width={200}
                   height={200}
+                  style={{'margin': 'auto'}}
                 />
               </Link>
             </div>
             <div className="product-info flex justify-between flex-col">
               <div className="flex justify-center flex-col">
-                <p className="text-xs text-gray-500">{props.product.brand?.name}</p>
-                <Link href={`/shop/product/${props.product.slug}`}><p className="text-lg font-bold">{props.product.name}</p></Link>
+                <p className="text-green-500 text-sm font-bold my-1">{props.product.brand?.name}</p>
+                <Link href={`/shop/product/${props.product.slug}`}><p className="text-lg font-bold mb-1">{props.product.name}</p></Link>
                 <p className="text-xs text-gray-500"><span className="font-bold">THC</span>: {props.product.potencyThc.formatted} | <span className="font-bold">CBD</span>: {props.product.potencyCbd.formatted}</p>
               </div>
-              <div className="product-attributes w-full xl:w-auto mt-2 flex justify-between">
-                <div className="text-xl flex justify-center items-center">
-                  {
-                    selectedVariant.specialPriceMed ? (
-                      <>
-                        <span className="font-bold">
-                          ${selectedVariant.specialPriceMed}
-                        </span>
-                        <span className="line-through	ml-5 text-lg">
-                          ${selectedVariant.priceMed}
-                        </span>
-                      </>
-                    ) : (
+            </div>
+            <div className="product-attributes w-full xl:w-auto mt-2 flex justify-between">
+              <div className="text-xl flex justify-center items-center">
+                {
+                  selectedVariant.specialPriceMed ? (
+                    <>
                       <span className="font-bold">
+                        ${selectedVariant.specialPriceMed}
+                      </span>
+                      <span className="line-through	ml-5 text-lg">
                         ${selectedVariant.priceMed}
                       </span>
-                    )
-                  }
-                </div>
-                <Select
-                  onChange={(v: string) => selectVariant(v)}
-                  options={options}
-                  className="w-[100px] rounded-lg"
-                  name="category_id"
-                  />
-                <button className="bg-black p-3 font-xl text-white rounded-[50px]" onClick={addToCart} type="button">
-                  <FontAwesomeIcon icon={faCartPlus} />
-                </button>
+                    </>
+                  ) : (
+                    <span className="font-bold">
+                      ${selectedVariant.priceMed}
+                    </span>
+                  )
+                }
               </div>
+              <Select
+                onChange={(v: string) => selectVariant(v)}
+                options={options}
+                className="w-[100px] rounded-lg product-options"
+                name="category_id"
+                />
+              <button className="btn-add-cart bg-black p-3 font-xl text-white rounded-[50px] flex items-center justify-cneter" onClick={addToCart} type="button">
+                <FontAwesomeIcon icon={faCartPlus} />
+              </button>
             </div>
           </>
       }
