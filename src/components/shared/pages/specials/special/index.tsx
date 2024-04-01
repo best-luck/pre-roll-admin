46fbff4db12156154c4501499acda5a075f6ff2a
unknown <webmaster0407@gmail.com>
2024-04-02 04:41:36 +0900

    specials layout fix 2

--- src/components/shared/pages/specials/special/index.tsx
+++ src/components/shared/pages/specials/special/index.tsx
@@ -7,22 +7,22 @@ import Link from "next/link";
 
 export default function Special({special, type}: {special: SpecialType, type?: string}) {
   return (
-    <div className={`flex justify-center items-center flex-col ${type==="view"?"w-full":"specials-grid"} bg-cover bg-no-repeat bg-center rounded-[20px] p-5 text-white h-[200px]`} style={{backgroundImage: `linear-gradient(90deg, rgba(11, 31, 50, 0.38) 0%, rgb(11, 31, 50) 100%), url(${SPECIAL_IMAGE_HOST}${special.menuDisplayConfiguration.image})`}}>
-      {
-       type==="view" ?
-        <>
-          <p className="text-xl font-bold">{special.menuDisplayConfiguration.name}</p>
-          <p className="text-md mt-5">{special.menuDisplayConfiguration.description}</p>
-        </> :
-        <>
-          <div className="specials-grid-inner">
+    <div className={`flex justify-center items-center flex-col ${type==="view"?"w-full":"specials-grid"}`}>
+      <div className={`bg-cover bg-no-repeat bg-center rounded-[20px] p-5 text-white h-[200px]`} style={{backgroundImage: `linear-gradient(90deg, rgba(11, 31, 50, 0.38) 0%, rgb(11, 31, 50) 100%), url(${SPECIAL_IMAGE_HOST}${special.menuDisplayConfiguration.image})`}}>
+        {
+        type==="view" ?
+          <>
+            <p className="text-xl font-bold">{special.menuDisplayConfiguration.name}</p>
+            <p className="text-md mt-5">{special.menuDisplayConfiguration.description}</p>
+          </> :
+          <>
             <p className="text-md font-bold">{special.menuDisplayConfiguration.name}</p>
             <Link href={`/deals/${special.id}`} className="mt-5">
               <span className="text-sm">Shop Now</span> <FontAwesomeIcon icon={faArrowRight} />
             </Link>
-          </div>
-        </>
-      }
+          </>
+        }
+      </div>
     </div>
   )
 }
