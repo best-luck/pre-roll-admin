b19ea22746165fc94568154387eb0523da11266b
Ubuntu <user@user-ubuntu>
2024-04-01 16:17:24 -0400

    updates

--- app/deals/[specialId]/page.tsx
+++ app/deals/[specialId]/page.tsx
@@ -1,5 +1,6 @@
 import { filterRetailerProductsAction } from "@src/lib/actions/products";
 import { getRetailerSpecials } from "@src/lib/dutchie/retailers";
+import { ProductType } from "@src/lib/types/product";
 import { SpecialType } from "@src/lib/types/specials";
 import Board from "app/shop/category/[category]/board";
 
@@ -13,11 +14,12 @@ export default async function Page({ params: { specialId } }: { params: { specia
   const specials: SpecialType[] = await getRetailerSpecials();
   const special = specials.find(s => s.id===specialId);
   const res = await filterRetailerProductsAction("Specials", "", "", [], [], [], [specialId], "");
+  const products = res.products.filter((product: ProductType) => product.variants[0].specialPriceMed)
 
   return (
     <div className="container m-auto py-10">
       <Board
-        products={res.products}
+        products={products}
         category="Specials"
         specials={specials}
         special={special}
--- app/deals/page.tsx
+++ app/deals/page.tsx
@@ -1,23 +1,32 @@
 import Specials from "@src/components/shared/pages/specials";
 import { filterRetailerProductsAction } from "@src/lib/actions/products";
 import { getRetailerSpecials } from "@src/lib/dutchie/retailers";
+import { ProductType } from "@src/lib/types/product";
 import { SpecialType } from "@src/lib/types/specials";
 import Board from "app/shop/category/[category]/board";
 
 export default async function Page() {
   const specials: SpecialType[] = await getRetailerSpecials();
-  const specialIds = specials.map(s => s.id)
-  const res = await filterRetailerProductsAction("Specials", "", "", [], [], [], specialIds, "");
+  const availableSpecials: SpecialType[] = [];
+  const products: ProductType[] = [];
+  for (const special of specials) {
+    const res = await filterRetailerProductsAction("Specials", "", "", [], [], [], [special.id], "");
+    const _products = res.products.filter((product: ProductType) => product.variants[0].specialPriceMed)
+    if (_products.length) {
+      availableSpecials.push(special);
+      products.push(..._products);
+    }
+  }
 
   return (
     <div className="container m-auto py-10">
       <Specials
-        specials={specials}
+        specials={availableSpecials}
         />
       <Board
-        products={res.products}
+        products={products}
         category="Specials"
-        specials={specials}
+        specials={availableSpecials}
         />
     </div>
   )
